6a591f5517ff400aa94aa65d058f0d7df7dcdb6b
WP-Studio01 <98379477+WP-Studio01@users.noreply.github.com>
2022-12-19 14:45:47 +0800

    构建主页

---
+++ render-gui.jsx
@@ -0,0 +1,33 @@
+function Nav(props) {
+    return (
+        <div>
+            <a className="navitem" href="#/home">主页</a>
+            <a className="navitem" href="#/blog">博客</a>
+            <a className="navitem github" href="?">Github</a>
+        </div>
+    );
+}
+function Text(props) {
+    return (
+        <div>
+            {location.hash=='#/home'?(
+                <div>
+                    <h1>这里是HelloWorld的新博客网站</h1>
+                    <p>本站使用React框架编写,我也是才学React,高手请多多指教</p>
+                </div>
+            ):(
+                <p>404 Not Found</p>
+            )}
+        </div>
+    );
+}
+
+(window.onhashchange=function() {
+    ReactDOM.render(
+        <div>
+            <Nav />
+            <center><Text /></center>
+        </div> ,
+        document.getElementById('content')
+    )
+})();
