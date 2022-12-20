82277e07484405dd01f05acdec42a3c91c491b58
WP-Studio01 <98379477+WP-Studio01@users.noreply.github.com>
2022-12-20 09:39:26 +0800

    增加Github账号Token接口（内容还没做）

--- render-gui.jsx
+++ render-gui.jsx
@@ -7,6 +7,7 @@ function Nav(props) {
             <a className="navitem" href="#/repo">存储库</a>
             <a className="navitem" href="#/code">代码</a>
             <a className="navitem github" href="https://github.com/WP-Studio01/NewBlog">Github</a>
+            <a className="navitem github" href="javascript:void(0)" onClick={function(){prompt('请输入你的Github账号Token,在访问博客时使用','请输入')}}>登录/修改Github账号</a>
         </div>
     );
 }
