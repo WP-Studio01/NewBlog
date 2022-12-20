e0008e2f972b83db121d83d6b406dbc32a1494bc
WP-Studio01 <98379477+WP-Studio01@users.noreply.github.com>
2022-12-20 11:11:55 +0800

    为博客页面渲染导航栏；把导航栏放入独立文件中维护

--- render-gui.jsx
+++ render-gui.jsx
@@ -1,23 +1,10 @@
-//导航栏
-function Nav(props) {
-    return (
-        <div>
-            <a className="navitem" href="#/home">主页</a>
-            <a className="navitem" href="#/blog">博客</a>
-            <a className="navitem" href="#/repo">存储库</a>
-            <a className="navitem" href="#/code">代码</a>
-            <a className="navitem github" href="https://github.com/WP-Studio01/NewBlog">Github</a>
-            <a className="navitem github" href="javascript:void(0)" onClick={function(){prompt('请输入你的Github账号Token,在访问博客时使用','请输入')}}>登录/修改Github账号</a>
-        </div>
-    );
-}
 //主页
 function Home(props) {
     return (
         <div>
             <h1>这里是HelloWorld的新博客网站</h1>
             <p>本站使用React框架编写,我也是才学React,高手请多多指教</p>
-            <p>我的博客被<img src="https://saobby.pythonanywhere.com/api/webcounter?id=bcbcj76C3lXbkRYu" />次访问过</p>
+            <p>我的博客被<img src="https://saobby.pythonanywhere.com/api/webcounter?id=bcbcj76C3lXbkRYu" />次访问</p>
         </div>
     );
 }
