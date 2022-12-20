625745218fbdef76d600cbe5222fc663cf9a665a
WP-Studio01 <98379477+WP-Studio01@users.noreply.github.com>
2022-12-20 12:27:01 +0800

    标题特效

---
+++ title.js
@@ -0,0 +1,14 @@
+// 浏览器标题切换  
+var OriginTitile = document.title;    // 保存之前页面标题  
+var titleTime;  
+document.addEventListener('visibilitychange', function(){  
+    if (document.hidden){  
+        document.title = '快点回来!!!';  
+        clearTimeout(titleTime);  
+    }else{  
+        document.title = '终于回来';  
+        titleTime = setTimeout(function() {  
+            document.title = window.OriginTitile;  
+        }, 3000); // 6秒后恢复原标题  
+    }  
+});  
